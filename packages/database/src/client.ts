f304b5fb21628bfef1e86c96a43463660a2d35f1
Muhammad Ahmed <ahmedaries.97@gmail.com>
2026-08-18 14:55:37 +0500

    Updated Database url thing issue

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,3 +1,9 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { config } from "dotenv";
+
+config({ path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../../../.env.local') });
+
 import {
   PrismaPg,
 } from "@prisma/adapter-pg";
